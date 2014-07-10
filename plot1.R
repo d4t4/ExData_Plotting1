82869cd6cea060d21a8b34cd571e2758e86d174f
d4t4 <d4t4@users.noreply.github.com>
2014-07-09 21:17:14 -0400

    Cleaned up formatting in plot1.R

--- plot1.R
+++ plot1.R
@@ -1,21 +1,21 @@
 load_data <- read.table(file = "household_power_consumption.txt",
-								sep = ";",
-								skip = 66637,
-								nrows = 2880,
-								na.strings= "?")
+			sep = ";",
+			skip = 66637,
+			nrows = 2880,
+			na.strings= "?")
 
 dataNames <- colnames(read.table("household_power_consumption.txt", 
-							sep = ";",
-							nrow = 1,
-							header = TRUE))
+			sep = ";",
+			nrow = 1,
+			header = TRUE))
 
 names(load_data) <- dataNames
 str(load_data)
 png("plot1.png", width=480, height=480)
 
 hist(load_data$Global_active_power,
-		col="red", 
-		main ="Global Active Power",
-		xlab="Global Active Power (kilowatts)")
+	col="red", 
+	main ="Global Active Power",
+	xlab="Global Active Power (kilowatts)")
 
 dev.off()
