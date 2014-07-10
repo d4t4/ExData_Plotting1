c9bb1aa3cf47c67cc54dd7852cf6d3856758002e
D4t4 Student <d4t4.student@gmail.com>
2014-07-09 20:44:16 -0400

    Added Plot 2

---
+++ plot2.R
@@ -0,0 +1,27 @@
+load_data <- read.table(file = "household_power_consumption.txt",
+								sep = ";",
+								skip = 66637,
+								nrows = 2880,
+								na.strings= "?")
+
+dataNames <- colnames(read.table("household_power_consumption.txt", 
+							sep = ";",
+							nrow = 1,
+							header = TRUE))
+
+names(load_data) <- dataNames
+str(load_data)
+
+dateTime <- paste(load_data$Date, load_data$Time)
+weekDay <- strptime(dateTime, format ='%d/%m/%Y %H:%M:%S')
+str(weekDay)
+
+png("plot2.png", width =480, height=480)
+
+
+plot(weekDay, load_data$Global_active_power,
+		type="l",
+		xlab="",
+		ylab="Global Active Power(kilowatts)")
+
+dev.off()
