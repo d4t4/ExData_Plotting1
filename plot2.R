9b2a613c22890865a3e412d26e8cca59b9c9d3e0
d4t4 <d4t4@users.noreply.github.com>
2014-07-09 21:16:32 -0400

    Cleaned up formatting in plot2.R

--- plot2.R
+++ plot2.R
@@ -1,13 +1,13 @@
 load_data <- read.table(file = "household_power_consumption.txt",
-								sep = ";",
-								skip = 66637,
-								nrows = 2880,
-								na.strings= "?")
+			sep = ";",
+			skip = 66637,
+			nrows = 2880,
+			na.strings= "?")
 
 dataNames <- colnames(read.table("household_power_consumption.txt", 
-							sep = ";",
-							nrow = 1,
-							header = TRUE))
+			sep = ";",
+			nrow = 1,
+			header = TRUE))
 
 names(load_data) <- dataNames
 str(load_data)
@@ -20,8 +20,8 @@ png("plot2.png", width=480, height=480)
 
 
 plot(weekDay, load_data$Global_active_power,
-		type="l",
-		xlab="",
-		ylab="Global Active Power(kilowatts)")
+	type="l",
+	xlab="",
+	ylab="Global Active Power(kilowatts)")
 
 dev.off()
