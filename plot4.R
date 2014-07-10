a7163690446b679429ae3d6cce924f3de42e7daa
D4t4 Student <d4t4.student@gmail.com>
2014-07-09 21:03:50 -0400

    Added Plot 4

---
+++ plot4.R
@@ -0,0 +1,45 @@
+load_data <- read.table(file = "household_power_consumption.txt",
+								sep = ";",
+								skip = 66637,
+								nrows = 2880,
+								na.strings= "?")
+
+dataNames <- colnames(read.table("household_power_consumption.txt", 
+							sep = ";",
+							nrow = 1,
+							header = TRUE))
+
+names(load_data) <- dataNames
+str(load_data)
+
+dateTime <- paste(load_data$Date, load_data$Time)
+weekDay <- strptime(dateTime, format ='%d/%m/%Y %H:%M:%S')
+str(weekDay)
+
+png("plot4.png", width=480, height=480)
+par(mfrow=c(2,2))
+
+plot(weekDay,load_data$Global_active_power,
+		type="l",
+		xlab="",
+		ylab="Global Active Power")
+plot(weekDay,load_data$Voltage,
+		type="l",
+		xlab="datetime",
+		ylab="Voltage")
+plot(weekDay,load_data$Sub_metering_1,
+		type="l",
+		xlab="",
+		ylab="Energy sub metering")
+lines(weekDay,load_data$Sub_metering_2, col= "red")
+lines(weekDay,load_data$Sub_metering_3, col= "blue")
+legend("topright",
+		lty=1,
+		bty ="n",
+		col=c("black","red","blue"),
+		legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+plot(weekDay,load_data$Global_reactive_power,
+		type="l",
+		xlab="datetime",
+		ylab="Global_reactive_power")
+dev.off()
