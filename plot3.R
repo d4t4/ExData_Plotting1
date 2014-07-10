6b919586a2184bd7dda51c6d33ef930d5c77c5fe
d4t4 <d4t4@users.noreply.github.com>
2014-07-09 21:15:37 -0400

    Cleaned up formatting in plot3.R

--- plot3.R
+++ plot3.R
@@ -1,13 +1,13 @@
 load_data <- read.table(file = "household_power_consumption.txt",
-								sep = ";",
-								skip = 66637,
-								nrows = 2880,
-								na.strings= "?")
+			sep = ";",
+			skip = 66637,
+			nrows = 2880,
+			na.strings= "?")
 
 dataNames <- colnames(read.table("household_power_consumption.txt", 
-							sep = ";",
-							nrow = 1,
-							header = TRUE))
+			sep = ";",
+			nrow = 1,
+			header = TRUE))
 
 names(load_data) <- dataNames
 str(load_data)
@@ -18,15 +18,15 @@ str(weekDay)
 
 png("plot3.png", width=480, height=480)
 plot(weekDay, load_data$Sub_metering_1,
-		type="l",
-		xlab="",
-		ylab="Energy sub metering")
+	type="l",
+	xlab="",
+	ylab="Energy sub metering")
 
 lines(weekDay, load_data$Sub_metering_2, col="red")
 lines(weekDay, load_data$Sub_metering_3, col="blue")
 legend("topright",
-		lty=1,
-		col=c("black","red","blue"),
-		legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+	lty=1,
+	col=c("black","red","blue"),
+	legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
 dev.off()
